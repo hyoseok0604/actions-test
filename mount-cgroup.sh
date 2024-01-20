cb4850eb0c090315ee55408cc86a9e0732a3aeca
hyoseok0604 <janghs0604@gmail.com>
2024-01-20 23:03:54 +0900

    cgroup v2

--- mount-cgroup.sh
+++ mount-cgroup.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 
-if [ ! -d "/cgroups" ]; then
-  mkdir /cgroups
+# Mount cgroup v2
+if [ ! -d "/cgroup" ]; then
+  mkdir /cgroup
 fi
 
-mount -t tmpfs cgroup_root /cgroups
-
-if [ ! -d "/cgroups/memory" ]; then
-  mkdir /cgroups/memory
-fi
-
-mount -t cgroup -o memory memory /cgroups/memory
-chown -hR test:test /cgroups/memory
-chmod -R +w /cgroups/memory
-
-if [ ! -d "/cgroups/cpuacct" ]; then
-  mkdir /cgroups/cpuacct
-fi
-
-mount -t cgroup -o cpuacct cpuacct /cgroups/cpuacct
-chown -hR test:test /cgroups/cpuacct
-chmod -R +w /cgroups/cpuacct
+mount -t cgroup2 none /cgroup
 
 mount
 
